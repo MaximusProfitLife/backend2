044f6b80a5f639032ea817b494de65ab8f5e4906
MaximusProfitLife <duacusticproducciones@gmail.com>
2026-07-10 17:49:39 -0400

    Reparacion y actualizacion de estructura

--- interes_bot.py
+++ interes_bot.py
@@ -23,8 +23,9 @@ for name in EXCHANGES_CONFIG:
             'enableRateLimit': True,
             'options': {'defaultType': 'future'},
             'headers': {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
-            }
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+    'Referer': 'https://www.binance.com/' 
+}
         })
     except Exception as e:
         print(f"❌ Error en {name}: {e}")
